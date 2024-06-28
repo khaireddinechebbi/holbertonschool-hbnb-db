33358e865a3cb3d1683b3ce459d583742191edb2
essioui <salahessioui@gmail.com>
2024-06-28 01:58:47 +0100

    requi

--- app.py
+++ app.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-app.config['USE_DATABASE'] = True 
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['USE_DATABASE'] = True
+    db.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
+    return app
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-
-db = SQLAlchemy(app)
-
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
-
-
-@app.route('/')
-def index():
-    return ' hello sqlachemy!'
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import os
+
+class Config(object):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
